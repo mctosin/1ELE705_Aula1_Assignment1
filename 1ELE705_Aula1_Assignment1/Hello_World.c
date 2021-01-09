09e08f9da0f1d6b7cb50a147c1683d8e3974abf4
mctosin <mctosin@gmail.com>
2021-01-09 14:40:32 -0300

    ressetando o projeto para criar um na mesma pasta

--- 1ELE705_Aula1_Assignment1/Hello_World.c
+++
@@ -1,9 +0,0 @@
-// 1ELE705_Aula1_Assigment1
-// Crie um projeto em branco e faça o programa imprimir no console o famoso "Hello World"
-
-#include "stdio.h"
-
-int main() {
-	printf("Hello World\n");
-	return 0;
-}
