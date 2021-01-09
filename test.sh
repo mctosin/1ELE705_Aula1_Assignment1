da35271f2c177078979638a874c50b80de2c785e
francneto <39171849+francneto@users.noreply.github.com>
2021-01-09 00:07:15 -0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 echo "Running tests..."
 echo
 
-output=$(./1ELE705_Aula1_Assignment1/Hello_World.out)
+output=$(./1ELE705_Aula1_Assignment1/Hello_World.o)
 expected_output="Hello World\n"
 
 if [ $? -eq 0 ] ; then
